12cf31503c7d9a3c858dcb651328b111a750a15f
gilbert <gilbert@cctalent.cc>
2024-08-04 00:00:22 +0800

    added allowed ip's

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
+require('dotenv').config(); // Load environment variables from .env file
+
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8888;
 
 var cors_proxy = require('cors-anywhere');
+
+// Read allowed IPs from environment variable and split into an array
+var allowedIPs = process.env.ALLOWED_IPS ? process.env.ALLOWED_IPS.split(',') : [];
+
 cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
+    originWhitelist: allowedIPs, // Allow only specified IP addresses
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
